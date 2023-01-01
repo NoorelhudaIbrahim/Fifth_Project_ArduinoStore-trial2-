a5d77ff98dc9cae4faae0a82be69b4481693223b
Huda Al-Ausaily <hudaausaily@gmail.com>
2023-01-01 21:19:33 +0300

    Landing style

--- home.php
+++ home.php
@@ -29,9 +29,12 @@ include 'components/wishlist_cart.php';
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
+   
+   <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Seymour+One&display=swap" rel="stylesheet">
+   
 
 </head>
-<body>
+<body style="background-image: url('images/bg2.jpg'); background-size:cover; ">
    
 <?php include 'components/user_header.php'; ?>
 
@@ -45,33 +48,33 @@ include 'components/wishlist_cart.php';
 
       <div class="swiper-slide slide">
          <div class="image">
-            <img src="images/home-img-1.png" alt="">
+            <img src="images/1.png" alt="">
          </div>
          <div class="content">
-            <span>upto 50% off</span>
-            <h3>latest smartphones</h3>
+            <span>up to 50% off</span>
+            <h3>Discover the power of AI</h3>
             <a href="shop.php" class="btn">shop now</a>
          </div>
       </div>
 
       <div class="swiper-slide slide">
          <div class="image">
-            <img src="images/home-img-2.png" alt="">
+            <img src="images/2.png" alt="0">
          </div>
          <div class="content">
-            <span>upto 50% off</span>
-            <h3>latest watches</h3>
+            <span>up to 50% off</span>
+            <h3>AI is going to change the world!</h3>
             <a href="shop.php" class="btn">shop now</a>
          </div>
       </div>
 
       <div class="swiper-slide slide">
          <div class="image">
-            <img src="images/home-img-3.png" alt="">
+            <img src="images/3.png" alt="">
          </div>
          <div class="content">
             <span>upto 50% off</span>
-            <h3>latest headsets</h3>
+            <h3>From human to machine and back again!</h3>
             <a href="shop.php" class="btn">shop now</a>
          </div>
       </div>
@@ -85,42 +88,62 @@ include 'components/wishlist_cart.php';
 </section>
 
 </div>
+<div >
 
-<section class="category">
+<section class="category" >
 
    <h1 class="heading">shop by category</h1>
 
    <div class="swiper category-slider">
 
-      <div class="swiper-wrapper">
+   <div class="swiper-wrapper">
 
-         <?php
-         $select_category = $conn->prepare("SELECT * FROM `category` LIMIT 6"); 
-         $select_category->execute();
-         if($select_category->rowCount() > 0){
-            while($fetch_category = $select_category->fetch(PDO::FETCH_ASSOC)){
-         ?>
-         <form action="" method="post" class="swiper-slide slide">
-            <input type="hidden" name="pid" value="<?= $fetch_category['category_id']; ?>">
-            <input type="hidden" name="name" value="<?= $fetch_category['category_name']; ?>">
-            <input type="hidden" name="description" value="<?= $fetch_category['category_description']; ?>">
-            <input type="hidden" name="image" value="<?= $fetch_category['category_imge']; ?>">
-            <img src="uploaded_img/<?= $fetch_category['category_imge']; ?>" alt="">
-            <div class="name"><?= $fetch_category['category_name']; ?></div>
-            <!-- <div class="flex">
-               <div class="price"><span>$</span><span>/-</span></div>
-               <input type="number" name="qty" class="qty" min="1" max="99" onkeypress="if(this.value.length == 2) return false;" value="1">
-            </div> -->
-         </form>
-         <?php
-            }
-         }else{
-            echo '<p class="empty">no category added yet!</p>';
-         }
-         ?>
+   <a href="category.php?category=laptop" class="swiper-slide slide">
+      <img src="images/icon-1.png" alt="">
+      <h3>laptop</h3>
+   </a>
 
-      </div>
+   <a href="category.php?category=tv" class="swiper-slide slide">
+      <img src="images/icon-2.png" alt="">
+      <h3>tv</h3>
+   </a>
+
+   <a href="category.php?category=camera" class="swiper-slide slide">
+      <img src="images/icon-3.png" alt="">
+      <h3>camera</h3>
+   </a>
+
+   <a href="category.php?category=mouse" class="swiper-slide slide">
+      <img src="images/icon-4.png" alt="">
+      <h3>mouse</h3>
+   </a>
+
+   <a href="category.php?category=fridge" class="swiper-slide slide">
+      <img src="images/icon-5.png" alt="">
+      <h3>fridge</h3>
+   </a>
+
+   <a href="category.php?category=washing" class="swiper-slide slide">
+      <img src="images/icon-6.png" alt="">
+      <h3>washing machine</h3>
+   </a>
+
+   <a href="category.php?category=smartphone" class="swiper-slide slide">
+      <img src="images/icon-7.png" alt="">
+      <h3>smartphone</h3>
+   </a>
+
+   <a href="category.php?category=watch" class="swiper-slide slide">
+      <img src="images/icon-8.png" alt="">
+      <h3>watch</h3>
+   </a>
+
    </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
 </section>
 
 <section class="home-products">
@@ -164,6 +187,7 @@ include 'components/wishlist_cart.php';
    <div class="swiper-pagination"></div>
 
    </div>
+</div>
 
 </section>
 
